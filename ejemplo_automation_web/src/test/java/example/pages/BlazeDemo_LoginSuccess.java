93bf73fa62b66b4a6b4271d4b3299b8fe1120ae0
francoGiulianini <franco.giulianini98@gmail.com>
2019-03-19 14:51:06 -0300

    Refactor pages - now using HashMap and Enum

--- ejemplo_automation_web/src/test/java/example/pages/BlazeDemo_LoginSuccess.java
+++ ejemplo_automation_web/src/test/java/example/pages/BlazeDemo_LoginSuccess.java
@@ -5,8 +5,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.HashMap;
+
 
 public class BlazeDemo_LoginSuccess extends WebComponent {
+
+    private HashMap<claves, By> loginPageMap = new HashMap();
+
+    public enum claves {
+        successMessage("successMessage");
+
+        private String clave;
+
+        claves(String clave) {
+            this.clave = clave;
+        }
+    }
+
+    public BlazeDemo_LoginSuccess()
+    {
+        loginPageMap.put(claves.successMessage, successMessage);
+    }
+
+    public By getLoginSuccessPageMap(claves clave) {
+        return loginPageMap.get(clave);
+    }
 
     public By successMessage = By.xpath("//div[@class='panel-body'][contains(text(),'You are logged in!')]");
 
